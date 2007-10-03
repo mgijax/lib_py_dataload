c4817327ffb1448409424a28e2637a7f83aeb902
David Miers <dave.miers@jax.org>
2007-10-03 14:40:44 +0000

    lib_py_dataload-3-5-4-1

--- gxdloadlib.py
+++ gxdloadlib.py
@@ -668,11 +668,10 @@ def verifyStructure(
         structureKey = structureDict[key]
     else:
 	results = db.sql('select s._Structure_key ' + \
-		'from GXD_Structure s, GXD_TheilerStage t, GXD_StructureName n ' + \
+		'from GXD_Structure s, GXD_TheilerStage t ' + \
 		'where s._Stage_key = t._Stage_key ' + \
 		'and t.stage = %s ' % (str(theilerStage)) + \
-		'and s._StructureName_key = n._StructureName_key ' + \
-		'and n.structure = "%s" ' % (structureName), 'auto')
+		'and s.printName = "%s" ' % (structureName), 'auto')
         if len(results) == 0:
 	    if errorFile != None:
                 errorFile.write('Invalid Structure (%d): %s:%s\n' % (lineNum, structureName, theilerStage))
