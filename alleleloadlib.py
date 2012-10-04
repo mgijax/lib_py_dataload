789980b118a081ee01daed38416854075c1051b0
Lori Corbani <Lori.Corbani@jax.org>
2012-10-04 16:53:44 +0000

    TR10273

--- alleleloadlib.py
+++ alleleloadlib.py
@@ -37,7 +37,8 @@ import db
 
 #globals
 
-mutantCellLineDict = {}      # mutant cell line
+mutantCellLineDict = {}      		# mutant cell line
+mutantCellLineByAlleleDict = {}		# mutant cell line by allele
 
 # Purpose:  verify Mutant Cell Line
 # Returns:  Mutant Cell Line key if valid, else 0
@@ -71,6 +72,45 @@ def verifyMutnatCellLine(
 	    for r in results:
                 mutantCellLineKey = r['_CellLine_key']
                 mutantCellLineDict[mutantCellLine] = mutantCellLineKey
+
+    return mutantCellLineKey
+
+# Purpose:  verify Mutant Cell Line by Allele
+# Returns:  Mutant Cell Line key for given Allele if valid, else 0
+# Assumes:  nothing
+# Effects:  verifies that the Mutant Cell Line exists for the given Allele 
+#	in the Mutant Cell Line dictionary
+#	writes to the error file if the Mutant Cell Line is invalid
+# Throws:  nothing
+
+def verifyMutnatCellLineByAllele(
+    mutantCellLine,      # mutant name (string)
+    alleleKey,      	 # allele key (string)
+    lineNum,		 # line number (integer)
+    errorFile            # error file (file descriptor)
+    ):
+
+    global mutantCellLineByAlleleDict
+
+    if mutantCellLineByAlleleDict.has_key(mutantCellLine):
+        mutantCellLineKey = mutantCellLineByAlleleDict[mutantCellLine]
+    else:
+	results = db.sql('''
+		select c._CellLine_key, c.cellLine 
+		from ALL_CellLine c, ALL_Allele_CellLine a
+		where c.cellLine = '%s'
+		and c.isMutant = 1
+		and c._CellLine_key = a._MutantCellLine_key
+		and a._Allele_key = %s
+		''' % (mutantCellLine, alleleKey), 'auto')
+        if len(results) == 0:
+	    if errorFile != None:
+                errorFile.write('Invalid Mutant CellLine (%d): %s\n' % (lineNum, mutantCellLine))
+            mutantCellLineKey = 0
+        else:
+	    for r in results:
+                mutantCellLineKey = r['_CellLine_key']
+                mutantCellLineByAlleleDict[mutantCellLine] = mutantCellLineKey
 
     return mutantCellLineKey
 
